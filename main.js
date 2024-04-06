6d6fbca76f02579c6eadeb8f57d6821848cc6bf5
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2024-04-06 09:19:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+function display(ID)
+{
+    document.getElementById(ID).style.display = 'block';
+}
+function hide(ID)
+{
+    document.getElementById(ID).style.display = 'none';
+}
+function createQR(option)
+{
+    if (option=="text"){
+        var content=document.getElementById("input_text").value;
+        var qrcode = new QRCode("qrcode", {
+            text: content,
+            width:480,
+            height:480,
+            colorDark:"#000000",
+            colorLight:"#ffffff",
+            correctLevel:QRCode.CorrectLevel.H
+        });
+    }
+    else if (option=="bank"){
+        var ngan_hang=document.getElementById("ngan_hang").value;
+        var STK=document.getElementById("STK").value;
+        var hovaten=document.getElementById("hovaten").value;
+        var ndcl=document.getElementById("ndcl").value;
+        var sotien=document.getElementById("sotien").value;
+        var link=`https://img.vietqr.io/image/${ngan_hang}-${STK}-print.png?amount=${sotien}&addInfo=${ndcl}&accountName=${hovaten}`;
+        console.log(link);
+        var img=document.getElementById("qrcode")
+        img.innerHTML=`<img src="${link}" alt="QR code">`;
+        console.log(img);
+    }
+    display("qrcode");
+    display("download");
+}
+function downloadQR()
+{
+    var canvas = document.getElementById("qrcode").querySelector('canvas');
+    var img = canvas.toDataURL("image/png");
+    var a = document.createElement('a');
+    a.href = img;
+    a.download = 'QRcode.png';
+    a.click();
+}
+async function getJSON(link){
+    try {
+        const response = await fetch(link);
+        if (response.status !== 200) {
+            alert("Error");
+        } else {
+            const x = await response.json();
+            return x;
+        }
+    }
+    catch (error) {
+        throw new Error("Cannot connect to " + link);
+        alert("Cannot connect to "+link);
+    }
+}   
+async function getAllBanks(){
+    var response= await getJSON('https://api.vietqr.io/v2/banks');
+    list=response.data;
+    var selectBox = document.getElementById('ngan_hang');
+    for(var i = 0; i < list.length; i++){
+        var option = list[i].shortName;
+        selectBox.options.add(new Option(option, option, false));
+    }
+} 
